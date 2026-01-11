193497c35498334f6ba09b4dac6986e24ff008a6
Chandrajith <chandruofficial97@gmail.com>
2026-01-12 02:11:24 +0530

    requirements

--- vtscloudsync/settings.py
+++ vtscloudsync/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 from pathlib import Path
 import environ
 import cloudinary
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -97,11 +98,23 @@ WSGI_APPLICATION = 'vtscloudsync.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+import dj_database_url
+import pymysql
+pymysql.install_as_MySQLdb()
+
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get("DATABASE_URL"),
+        engine='django.db.backends.mysql',
+    )
 }
 
 
